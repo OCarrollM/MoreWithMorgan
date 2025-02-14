98441c55c6062ab35291aeecd22ebab01ff59b13
ocarrolm <m.ocarroll@lancaster.ac.uk>
2025-02-14 15:19:16 +0000

    Small update, removed instagram to random womens page, added photo to top, editing big three into grid, made nav disapeer upon scroll

--- script.js
+++ script.js
@@ -1,24 +1,10 @@
-let lastScroll = 0;
-const navbar = document.querySelector('.navbar');
-
-window.addEventListener('scroll', () => {
-  const currentScroll = window.pageYOffset;
-  
-  // Always show navbar if user is at the very top of the page
-  if (currentScroll <= 0) {
-    navbar.classList.remove('.navbar-hide');
-    lastScroll = 0;
-    return;
-  }
-
-  if (currentScroll > lastScroll) {
-    // User scrolled down -- hide the navbar
-    navbar.classList.add('.navbar-hide');
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
   } else {
-    // User scrolled up -- show the navbar
-    navbar.classList.remove('.navbar-hide');
+    document.getElementById("navbar").style.top = "-50px";
   }
-
-  // Update lastScroll to the new position
-  lastScroll = currentScroll;
-});
+  prevScrollpos = currentScrollPos;
+}
