a87a185195308a1d73bc12a4625583fa2d8f22e7
ocarrolm <m.ocarroll@lancaster.ac.uk>
2025-02-14 17:38:57 +0000

    Fine tuning

--- script.js
+++ script.js
@@ -8,3 +8,51 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
+
+function readMoreBtnWed() {
+  var dots = document.getElementById("read");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+function readMoreBtnCar() {
+  var dots = document.getElementById("read2");
+  var moreText = document.getElementById("more2");
+  var btnText = document.getElementById("myBtn2");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+function readMoreBtnRep() {
+  var dots = document.getElementById("read3");
+  var moreText = document.getElementById("more3");
+  var btnText = document.getElementById("myBtn3");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
